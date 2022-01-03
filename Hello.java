ec4b106ca9cc9752bdacd356cc89914a49254c52
athallahdeiga <athallahdeiga26@gmail.com>
2022-01-03 15:24:41 +0700

    edit file

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ class Hello{
 		System.out.println("GTS: Aplikasi Java 2021");
 		System.out.println("--- *** ---");
 		System.out.println("END");
+System.out.println("athallah deiga dan ghupran trippy")
 	}
 }
